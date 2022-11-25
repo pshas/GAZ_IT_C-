aaaa896067f9f4103cff5d9f245e0e0c744a4214
Pavel Ptitsyn <ptitsyn.pasha@yandex.ru>
2022-11-25 15:56:19 +0300

    1

---
+++ Mod03/Starter/BlueYonder.Hotels.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace BlueYonder.Hotels.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage message = await client.GetAsync("http://localhost:5000/api/HotelBooking/1");
+                Console.WriteLine("Getting booking:");
+                string resultAsString = await message.Content.ReadAsStringAsync();
+                Console.WriteLine(resultAsString);
+
+                Booking booking = await message.Content.ReadAsAsync<Booking>();
+                booking.Paid = false; 
+                
+                message = await client.PutAsJsonAsync("http://localhost:5000/api/HotelBooking/1", booking);
+                resultAsString = await message.Content.ReadAsStringAsync();
+                Console.WriteLine("Booking after update:");
+                Console.WriteLine(resultAsString);
+            }
+        }
+    }
+}
